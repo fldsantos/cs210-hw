5188a746e9e3cda25995f52071baff42084ed1be
fldsantos <felipe.lpdsantos@gmail.com>
2024-01-09 16:33:59 -0300

    done prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,12 +8,26 @@ class Program
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         int userNumber = 1;
 
+        int sum = 0;
+        int average = 0;
+        int largest = 0;
+
         while (userNumber != 0) {
             Console.WriteLine("Enter a Number: ");
             userNumber = Convert.ToInt32(Console.ReadLine());
-            numbers.Append(userNumber);
+            numbers.Add(userNumber);
+        }
+        numbers.RemoveAt(numbers.Count - 1);
+
+        foreach (int number in numbers) {
+            sum += number;
         }
 
-        numbers.ForEach(Console.WriteLine);
+        average = sum / numbers.Count;
+        largest = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest is {largest}");
     }
 }
