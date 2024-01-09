3a75148514032bb5b5e898e0cc3c66250e709091
fldsantos <felipe.lpdsantos@gmail.com>
2024-01-09 16:56:12 -0300

    done prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,29 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        void DisplayWelcome() {
-            Console.WriteLine("Welcome to the Program!");
-        }
-
-        String PromptUserName() {
-            Console.WriteLine("Please enter your name: ");
-            return Console.ReadLine();
-        }
-
-        int PromptUserNumber() {
-            Console.WriteLine("Please enter your favorite number: ");
-            return Convert.ToInt32(Console.ReadLine());
-        }
-
-        int SquareNumber (int number) {
-            return  Convert.ToInt32(Math.Pow(number, 2));
-        }
-
-        void DisplayResult(String name, int number) {
-            Console.WriteLine($"{name}, the square of your number is {SquareNumber(number)}");
-        }
-        
         DisplayWelcome();
         DisplayResult(PromptUserName(), PromptUserNumber());
     }
-}   
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return Convert.ToInt32(Math.Pow(number, 2));
+    }
+
+    static void DisplayResult(String name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {SquareNumber(number)}");
+    }
+}
