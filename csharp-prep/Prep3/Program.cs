88ae50032a7317f86cf72ac25670d2c2ec2705ce
fldsantos <felipe.lpdsantos@gmail.com>
2024-01-09 15:56:47 -0300

    done prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int userNumber = 0;
+
+        Console.WriteLine($"{number} I'm thinking of a magic number! Can you guess it?");
+        userNumber = Convert.ToInt32(Console.ReadLine());
+
+        while (userNumber != number) {
+            if (number > userNumber) {
+                Console.WriteLine("Higher! Try Again.");
+                userNumber = Convert.ToInt32(Console.ReadLine());
+            } else {
+                Console.WriteLine("Lower! Try Again.");
+                userNumber = Convert.ToInt32(Console.ReadLine());
+            }
+        } 
+
+        Console.WriteLine("You Guessed It!");
     }
 }
