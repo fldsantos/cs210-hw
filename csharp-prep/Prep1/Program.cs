dbed5ed92698c01f3c9617c7920270714b14901a
fldsantos <felipe.lpdsantos@gmail.com>
2024-01-09 15:05:02 -0300

    done prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        String fName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        String lName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
