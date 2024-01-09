3f22548563047344349aff8b8f6ac9ae446663d9
fldsantos <felipe.lpdsantos@gmail.com>
2024-01-09 15:36:29 -0300

    Done with prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        int score = Convert.ToInt32(Console.ReadLine());
+        String grade = "";
+
+        if (score >= 90) {
+            grade = "A";
+        } else if (score >= 80) {
+            grade = "B";
+        } else if (score >= 70) {
+            grade = "C";
+        } else if (score >= 60) {
+            grade = "D";
+        } else if (score < 60) {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Grade: {grade}");
+
+        if(score >= 70) {
+            Console.WriteLine("Congratulations! You passed!");
+        } else {
+            Console.WriteLine("Unfortunately, you were not approved.");
+        }
+        
     }
 }
