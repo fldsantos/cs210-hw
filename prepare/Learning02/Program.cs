c7b28f61419d637d776e0ddb6a4784db87a1666a
fldsantos <felipe.lpdsantos@gmail.com>
2024-01-15 23:24:32 -0300

    done resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job myJob = new Job();
+        myJob._jobTitle = "Software Engineer";
+        myJob._company = "Google";
+        myJob._endYear = 2020;
+        myJob._startYear = 2018;
+
+        Job myJob2 = new Job();
+        myJob2._jobTitle = "Software Engineer";
+        myJob2._company = "Meta";
+        myJob2._endYear = 2024;
+        myJob2._startYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Felipe";
+        myResume._jobs.Add(myJob);
+        myResume._jobs.Add(myJob2);
+
+        myResume.Display();
     }
 }
