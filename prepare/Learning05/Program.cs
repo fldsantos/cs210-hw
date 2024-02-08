f7ed3425623468a860ea34005bb8caa4744daefc
fldsantos <felipe.lpdsantos@gmail.com>
2024-02-08 14:40:51 -0300

    i did it

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 2));
+        shapes.Add(new Rectangle("green", 2, 3));
+        shapes.Add(new Circle("blue", 3));
+
+        for(int i = 0; i < shapes.Count ; i++) {
+            Console.WriteLine(shapes[i].GetColor());
+            Console.WriteLine(shapes[i].GetArea());
+        }
+    
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,13 @@
+class Rectangle : Shape  {
+
+    private double _length;
+    private double _width;
+    public Rectangle(string color, double width, double length): base(color) {
+        _width = width;
+        _length = length;
+    }
+
+    public override double GetArea(){
+        return _length*_width;
+    }
+}
